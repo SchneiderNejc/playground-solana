6dc2b9e5b308e09e9c33e6b038c9b80d1226e404
Nejc Schneider <nejc.sch@gmail.com>
2024-09-26 14:17:21 +0200

    init Game storage

--- tic-tac-toe/programs/tic-tac-toe/src/lib.rs
+++ tic-tac-toe/programs/tic-tac-toe/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("EnXiwvsvV9c9p2yggdvzaBuAuGAms2smeqQeyEZB7Zmy");
 
 #[program]
 pub mod tic_tac_toe {
@@ -13,3 +13,11 @@ pub mod tic_tac_toe {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account]
+pub struct Game {
+    players: [Pubkey; 2],          // (32 * 2)
+    turn: u8,                      // 1
+    board: [[Option<Sign>; 3]; 3], // 9 * (1 + 1) = 18
+    state: GameState,              // 32 + 1
+}
