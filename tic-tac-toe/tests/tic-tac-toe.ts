05f9b1891f6a8ea1572ecb391276cb41614f035e
Nejc Schneider <nejc.sch@gmail.com>
2024-09-27 14:06:35 +0200

    test "setup game!"

--- tic-tac-toe/tests/tic-tac-toe.ts
+++ tic-tac-toe/tests/tic-tac-toe.ts
@@ -8,9 +8,31 @@ describe("tic-tac-toe", () => {
 
   const program = anchor.workspace.TicTacToe as Program<TicTacToe>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("setup game!", async () => {
+    const gameKeypair = anchor.web3.Keypair.generate();
+    const playerOne = (program.provider as anchor.AnchorProvider).wallet;
+    const playerTwo = anchor.web3.Keypair.generate();
+    await program.methods
+      .setupGame(playerTwo.publicKey)
+      .accounts({
+        game: gameKeypair.publicKey,
+        playerOne: playerOne.publicKey,
+      })
+      .signers([gameKeypair])
+      .rpc();
+
+    let gameState = await program.account.game.fetch(gameKeypair.publicKey);
+    expect(gameState.turn).to.equal(1);
+    expect(gameState.players).to.eql([
+      playerOne.publicKey,
+      playerTwo.publicKey,
+    ]);
+    expect(gameState.state).to.eql({ active: {} });
+    expect(gameState.board).to.eql([
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ]);
+  });
   });
 });
