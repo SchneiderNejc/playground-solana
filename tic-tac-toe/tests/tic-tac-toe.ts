19b6e97cfdfc64806e45b9e1dd15c36e33a4e0fb
Nejc Schneider <nejc.sch@gmail.com>
2024-09-27 14:07:17 +0200

    import chai

--- tic-tac-toe/tests/tic-tac-toe.ts
+++ tic-tac-toe/tests/tic-tac-toe.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { TicTacToe } from "../target/types/tic_tac_toe";
+import { expect } from "chai";
 
 describe("tic-tac-toe", () => {
   // Configure the client to use the local cluster.
