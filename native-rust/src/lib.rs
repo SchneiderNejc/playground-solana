129d98afcbfd7a6995eb0485a991f8c37f915a7d
Nejc Schneider <nejc.sch@gmail.com>
2024-10-10 15:26:00 +0200

    native-rust: lib.rs

--- native-rust/src/lib.rs
+++ native-rust/src/lib.rs
@@ -1,14 +1,66 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program::invoke,
+    pubkey::Pubkey,
+    rent::Rent,
+    system_instruction::create_account,
+    sysvar::Sysvar,
+};
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let instruction = Instructions::try_from_slice(instruction_data)?;
+    match instruction {
+        Instructions::Initialize { data } => process_initialize(program_id, accounts, data),
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn process_initialize(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    data: u64,
+) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    let new_account = next_account_info(accounts_iter)?;
+    let signer = next_account_info(accounts_iter)?;
+    let system_program = next_account_info(accounts_iter)?;
+
+    let account_data = NewAccount { data };
+    let size = account_data.try_to_vec()?.len();
+    let lamports = (Rent::get()?).minimum_balance(size);
+
+    invoke(
+        &create_account(
+            signer.key,
+            new_account.key,
+            lamports,
+            size as u64,
+            program_id,
+        ),
+        &[signer.clone(), new_account.clone(), system_program.clone()],
+    )?;
+
+    account_data.serialize(&mut *new_account.data.borrow_mut())?;
+    msg!("Changed data to: {:?}!", data);
+    Ok(())
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub enum Instructions {
+    Initialize { data: u64 },
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct NewAccount {
+    pub data: u64,
 }
