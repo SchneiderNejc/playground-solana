e733139e6f4aa6979299d5e0ae31134de4b8372d
Nejc Schneider <nejc.sch@gmail.com>
2024-10-10 18:30:03 +0200

    native-rust: title section

--- native-rust/src/lib.rs
+++ native-rust/src/lib.rs
@@ -1,3 +1,4 @@
+// @title Simple Solana program with a single instruction that creates a new account.
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
