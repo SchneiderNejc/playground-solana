8567cf1fec4783f5eafa9d5900ffea97a53c14a1
Nejc Schneider <nejc.sch@gmail.com>
2024-10-16 15:38:28 +0200

    voting: only owner can InitializeCandidate

--- voting/programs/voting/src/lib.rs
+++ voting/programs/voting/src/lib.rs
@@ -2,8 +2,9 @@ use anchor_lang::prelude::*;
 
 declare_id!("GtZMCrJPXNu1UcDdvLK7Z1bYKJcJ8w7HWeWwxjGV3Xk4");
 
-#[program]
+const OWNER: &str = "8os8PKYmeVjU1mmwHZZNTEv5hpBXi5VvEKGzykduZAik";
 
+#[program]
 pub mod voting {
     use super::*;
 
@@ -14,17 +15,29 @@ pub mod voting {
     pub fn vote_for_candidate(ctx: Context<VoteCandidate>) -> Result<()> {
         Ok(())
     }
-
 }
 
-
+#[derive(Accounts)]
+pub struct InitializeCandidate<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+}
 
 #[derive(Accounts)]
+pub struct VoteCandidate {}
 
-pub struct InitializeCandidate{}
+fn check(ctx: &Context<InitializeCandidate>) -> Result<()> {
+    // Check if signer === owner
+    require_keys_eq!(
+        ctx.accounts.payer.key(),
+        OWNER.parse::<Pubkey>().unwrap(),
+        OnlyOwnerError::NotOwner
+    );
+    Ok(())
+}
 
-
-
-#[derive(Accounts)]
-
-pub struct VoteCandidate{}
+#[error_code]
+pub enum OnlyOwnerError {
+    #[msg("Only owner can call this function!")]
+    NotOwner,
+}
