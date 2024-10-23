c73570e9df2426dcc5fdbe76b50fa0a783ddaaed
Nejc Schneider <nejc.sch@gmail.com>
2024-10-23 16:57:09 +0200

    cpi-variants: use anchor_lang transfer

--- cpi-variants/programs/cpi-variants/src/lib.rs
+++ cpi-variants/programs/cpi-variants/src/lib.rs
@@ -1,15 +1,36 @@
 use anchor_lang::prelude::*;
+use anchor_lang::system_program::{transfer, Transfer};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("4fj6bhyRhn5oAejXEQRFvuTK45JUk4vLJG6TbvoxKbTw");
 
 #[program]
 pub mod cpi_variants {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn sol_transfer(ctx: Context<SolTransfer>, amount: u64) -> Result<()> {
+        let from_pubkey = ctx.accounts.sender.to_account_info();
+        let to_pubkey = ctx.accounts.recipient.to_account_info();
+        let program_id = ctx.accounts.system_program.to_account_info();
+
+        let cpi_context = CpiContext::new(
+            program_id,
+            Transfer {
+                from: from_pubkey,
+                to: to_pubkey,
+            },
+        );
+
+        transfer(cpi_context, amount)?;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
+pub struct SolTransfer<'info> {
+    #[account(mut)]
+    sender: Signer<'info>,
+    #[account(mut)]
+    recipient: SystemAccount<'info>,
+    system_program: Program<'info, System>,
+}
 pub struct Initialize {}
