d52264e5bd4c786df3496c98dfe06adb24b411b6
Nejc Schneider <nejc.sch@gmail.com>
2024-11-22 11:17:59 +0100

    move token-extensions to independent repo
    https://github.com/SchneiderNejc/token-extensions-solana

--- token-extensions/metadata-pointer/scripts/initConnection.js
+++
@@ -1,9 +0,0 @@
-const { Connection, clusterApiUrl, Keypair } = require("@solana/web3.js");
-const os = require("os");
-const fs = require("fs");
-
-const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-const secretKey = JSON.parse(fs.readFileSync(`${os.homedir()}/.config/solana/id.json`, "utf8"));
-const payer = Keypair.fromSecretKey(new Uint8Array(secretKey));
-
-module.exports = { connection, payer };
